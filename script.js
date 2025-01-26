c7669ad9e0f20ac42b8473cc2858419f4aaab77a
tjlearnscode <121098526+tjlearnscode@users.noreply.github.com>
2025-01-26 16:38:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const spriteSheet = new Image();
 spriteSheet.src = 'Breakout_Tile_Free.png';
 const starryBG = new Image();
 starryBG.src = 'starryBG.png';
+const bossImg = new Image();
+bossImg.src = 'boss.png';
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 const canvasHeight = canvas.height = 800;
 const canvasWidth = canvas.width = 1200;
-const brickWidth = 100;
+const brickWidth = 120;
 const brickHeight = 50;
 let brickCount = 0;
 let points =0;
@@ -18,22 +20,36 @@ let resetButton = document.getElementById("reset");
 let moveLeftButton = document.getElementById("moveLeft");
 let moveRightButton = document.getElementById("moveRight");
 let pointsContainer = document.getElementById("points");
-let levelNumber = 2;
+let levelNumber = 1;
 let level = levels.filter((e) => e.levelNumber === levelNumber)[0];
+let lasers = [];
+let ctrlKeyDown = false;
+let isRunning = false
 
 
 //CONTROLLER EVENTS
 let startX = 0;
-document.addEventListener('keydown', (event) => {player.handlePlayerKeyInput(event.key)});
+document.addEventListener('keyup',(event) => {
+  if(event.key === "Control"){
+    ctrlKeyDown = false;
+    console.log(ctrlKeyDown);
+  } else {}
+})
+document.addEventListener('keydown', (event) => {
+  if(event.key === "Control"){
+    ctrlKeyDown = true
+  } else if(event.key === "Enter") {
+    ball.moving = true;
+    console.log(ball)
+  } else {}
+  player.handleKeyInput(event.key); 
+  ball.handleKeyInput(event.key)
+});
 document.addEventListener('touchstart', event => {player.handleTouchStart(event)});
 document.addEventListener('touchmove', event => {player.handleTouchMove(event)});
 document.addEventListener('mousemove', event => {player.handleMouseMove(event)});
 canvas.addEventListener('click', startBall);
 canvas.addEventListener('touchStart', startBall);
-document.addEventListener('keydown', (event) => {console.log(event.key);if (event.key == "Enter") {
-  startBall()
-}
-});
 
 //SOUND EFFECTS AND MUSIC
 const backgroundMusic = document.getElementById('backgroundMusic');
@@ -43,7 +59,8 @@ const brickCollisionSoundEffect = document.getElementById('brickCollisionSoundEf
 const muteButton = document.getElementById('muteButton');
 
 //START BUTTON
-document.getElementById('start').addEventListener('click', gameLoop);
+document.getElementById('start').addEventListener('click', () => {isRunning = !isRunning; disableStart(); gameLoop()});
+document.getElementById('pause').addEventListener('click', pause);
 
 
 class Player {
@@ -82,7 +99,10 @@ class Player {
 }
 
   // Handle player input (e.g., keyboard, gamepad)
-  handlePlayerKeyInput(key) {
+  handleKeyInput(key) {
+    if(ctrlKeyDown) {
+      //do nothing if control key is down as it will be engaging the movement of the ball pointer
+    } else {
     switch(key) {
       case 'ArrowLeft': this.x -= this.speed;
         break;
@@ -91,6 +111,7 @@ class Player {
       default: {};
 };
   }
+}
 
   update() {
     this.imgReps ++;
@@ -155,8 +176,8 @@ class Brick {
 
 let bricksArr = [];
 
-for(let i = 0; i < 5; i++) {
-  for(let j = 0; j < 12; j++) {
+for(let i = 0; i < 1; i++) {
+  for(let j = 0; j < 10; j++) {
     const brick = new Brick (level.brickColor, i, j);
     bricksArr.push(brick);
   }
@@ -177,8 +198,6 @@ function drawBackground(img) {
 }
 
 
-
-
 class Ball {
   constructor() {
   this.ballCoords = {
@@ -192,22 +211,41 @@ class Ball {
   this.height = 25,
   this.radius = 12.5,
   this.xOrigin = player.xOrigin + ((player.width-this.width) / 2),
-  this.yOrigin = player.yOrigin - this.height,
+  this.yOrigin = player.yOrigin - this.height -1,
   this.x = this.xOrigin,
   this.y = this.yOrigin,
   this.centerX = this.x + this.radius,
   this.centerY = this.y + this.radius,
-  this.dx = 7,
-  this.dy = -7,
-  this.moving = false;
-  this.ms = 0;
-}
-
+  this.dx = 10,
+  this.dy = -10,
+  this.moving = false,
+  this.xDirection = 'straight',
+  this.ms = 0,
+  this.pointerPosIndex = 2,
+  this.pointerX = this.x + this.radius,
+  this.pointerY =this.y + this.radius,
+  this.pointerXEnd = this.pointerX,
+  this.pointerYEnd = this.pointerY - 50,
+  this.strokeColor = "rgba(243, 247, 246, 1)"
+  }
+  handleKeyInput(key) {
+    if(ctrlKeyDown) {
+      if(key === "ArrowRight" && this.pointerPosIndex < 3) {
+        this.pointerPosIndex ++;
+      } else if(key === "ArrowLeft" && this.pointerPosIndex > 1) {
+        this.pointerPosIndex --;
+        } else {}
+    } 
+    }
   update() {
+    
+
     if(this.x + this.width >= canvasWidth) {
       this.dx = -this.dx;
+      this.xDirection = 'left';
     } else if (this.x <= 0) {
       this.dx = -this.dx;
+      this.xDirection = 'right';
     }
 
     if (this.y <= 0) {
@@ -219,28 +257,85 @@ class Ball {
     if (this.moving) {
       this.x += this.dx;
       this.y += this.dy;
-      this.ms += 1;
+      this.strokeColor  = "rgba(243, 247, 246, 0)";
     } else {
       this.x = player.x + (player.width/2 - this.radius);
+      this.strokeColor  = "rgba(243, 247, 246, 1)";
+      switch(this.pointerPosIndex){
+        case 1: this.pointerXEnd = ball.x + ball.radius - 28;
+                this.pointerYEnd = ball.y -28;
+                this.dx = -10;
+                this.dy = -10;
+                this.xDirection = "left";
+          break;
+        case 2: this.pointerXEnd = ball.x + ball.radius;
+                this.pointerYEnd = ball.y -28;
+                this.dx = 0;
+                this.dy = -10;
+                this.xDirection = 'straight';
+          break;
+        case 3: this.pointerXEnd = ball.x + ball.radius + 28;
+                this.pointerYEnd = ball.y - 28;
+                this.dx = 10;
+                this.dy = -10;
+                this.xDirection = 'right';
+          break;
+        default: this.pointerXEnd = ball.x + ball.radius;
+          this.pointerYEnd = ball.y -28;
+          this.dx = 0;
+          this.dy = -10;
+          this.xDirection = 'straight';
+      }
+      this.pointerX = this.x + this.radius;
+      this.pointerY = this.y + this.radius;
     }
+    
   }
-
   draw(ctx) {
+    ctx.beginPath();
+    ctx.moveTo(this.pointerX, this.pointerY);
+    ctx.lineTo(this.pointerXEnd, this.pointerYEnd);
+    ctx.strokeStyle = this.strokeColor;
+    ctx.closePath();
+    ctx.stroke();
     ctx.drawImage(spriteSheet, this.ballCoords.sx, this.ballCoords.sy, this.ballCoords.sw, this.ballCoords.sh, this.x, this.y, this.width, this.height);
   }
 }
 
-
 let ball = new Ball;
+
 
 function checkBrickCollision() {
 
-    if (ball.ms > 5){
-      if (ball.x + ball.radius >= player.x && ball.x + ball.radius <= player.x + player.width && ball.y + ball.height === player.y) {
+  if (ball.x + ball.radius >= player.x && ball.x + ball.radius <= player.x + player.width && ball.y + ball.height === player.y + 9) {
+    if(ball.xDirection == 'right'){
+      if(ball.x + ball.radius < player.x + 50){
+        ball.dx = -ball.dx;
+    } else {
+
+    }
+    } else if(ball.xDirection == 'left'){
+      if(ball.x + ball.radius > player.x + 100){
+        ball.dx = -ball.dx;
+      } else {
+
+      }
+    } else {
+      if(ball.x + ball.radius < player.x + 50){
+        ball.dx = -10;
+        ball.xDirection = 'left';
+      } else if(ball.x + ball.radius > player.x + 50){
+        ball.dx = 10
+        ball.xDirection = 'right';
+      } else {
+
+      }
+    } 
     ball.dy = -ball.dy;
     paddleCollisionSoundEffect.play();
-      } else {};
-  } else {};
+    } else {
+  
+  }
 
 
   for(let i = 0; i < bricksArr.length; i++) {
@@ -248,11 +343,17 @@ function checkBrickCollision() {
         if (ball.y + ball.height >= bricksArr[i].destinationY & ball.y <= bricksArr[i].destinationY + bricksArr[i].height) {
           if (bricksArr[i].strength === 1) {
             bricksArr.splice(i, 1);
+            if(bricksArr.length === 0){
+              boss1.active = true;
+            }
           } else {
             bricksArr[i].strength --;
           }
           brickCollisionSoundEffect.play();
           points += 2;
+          if(Number.isInteger(points / 50)) {
+            lasers.push(new Laser(player.x, player.width, player.y));
+          }
           pointsContainer.innerText = "Points: " + points;
           countBricks();
           ball.dy = -ball.dy;
@@ -260,26 +361,64 @@ function checkBrickCollision() {
         } else if (ball.y + ball.radius >= bricksArr[i].destinationY & ball.y + ball.radius <= bricksArr[i].destinationY + bricksArr[i].height) {
           if (ball.x + ball.width >= bricksArr[i].destinationX & ball.x <= bricksArr[i].destinationX + bricksArr[i].width) {
             if (bricksArr[i].strength === 1) {
-              bricksArr.splice(i, 1)
+              bricksArr.splice(i, 1);
+              if(bricksArr.length === 0){
+                boss1.active = true;
+              }
             } else {
               bricksArr[i].strength --;
             }
             brickCollisionSoundEffect.play();
+            points += 2;
+          if(Number.isInteger(points / 50)) {
+            lasers.push(new Laser(player.x, player.width, player.y));
+          }
+          pointsContainer.innerText = "Points: " + points;
             countBricks();
             ball.dx = -ball.dx;
+            if(ball.xDirection == 'right'){
+              ball.xDirection = 'left';
+            } else if(ball.xDirection == 'left'){
+              ball.xDirection = 'right';
+            } else {
+
             }
           }
     }
 }
+}
 
+function checkBossCollision(){
+  if (ball.x + ball.radius >= boss1.x & ball.x + ball.radius <= boss1.x + boss1.width) {
+    if (ball.y + ball.height >= 0 & ball.y <= boss1.height){
+      boss1.strength--;
+      brickCollisionSoundEffect.play();
+      points += 2;
+      pointsContainer.innerText = "Points: " + points;
+      ball.dy = -ball.dy;
+    }
+} else if (ball.y + ball.radius >= 0 & ball.y + ball.radius <= boss1.height) {
+  if (ball.x + ball.width >= boss1.x & ball.x <= boss1.x + boss1.width){
+    boss1.strength--;
+    brickCollisionSoundEffect.play();
+    points += 2;
+    pointsContainer.innerText = "Points: " + points;
+    boss1.dx = -boss1.dx;
+    ball.dx = -ball.dx;
+  }
+}
+}
+
+function pause() {
+  isRunning = !isRunning;
+}
 
 function reset() {
-  // ball.moving = false;
-  // ball.x = ball.xOrigin;
-  // ball.y = ball.yOrigin;
-  // ball.dx = 7;
-  // ball.dy = -7;
-  ball = new Ball;
+  ball.moving = false;
+  ball.x = ball.xOrigin;
+  ball.y = ball.yOrigin;
+  ball.dx = 0;
+  ball.dy = 0;
   player.x = player.xOrigin;
   player.y = player.yOrigin;
 }
@@ -291,7 +430,6 @@ function startBall() {
 
 function countBricks() {
   brickCount = bricksArr.length;
-  console.log(brickCount);
 }
 
 class Laser {
@@ -323,19 +461,56 @@ class Laser {
 
 let laser1 = new Laser(player.x, player.width, player.y);
 
+function disableStart() {
+  document.getElementById('start').disabled = true;
+}
+
+class Boss  {
+  constructor(){
+  this.xOrigin = 400,
+  this.yOrigin = 0,
+  this.x = 400,
+  this.width = 400,
+  this.height = 150,
+  this.dx = 5,
+  this.strength = 3,
+  this.active = false
+  }
+
+  update(){
+    if(this.x <= 0 || this.x + this.width >= canvasWidth){
+      this.dx = -this.dx;
+    }
+    this.x += this.dx;
+    ctx.drawImage(bossImg,this.x,this.yOrigin,this.width, this.height);
+  }
+}
+
+const boss1 = new Boss
+
 function gameLoop() {
+  if(isRunning){
   backgroundMusic.play();
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
   drawBackground(starryBG);
-  player.update();
-  player.draw(ctx);
-  checkBrickCollision();
-  drawAllBricks(bricksArr);
-  laser1.update();
-  ball.update();
-  ball.draw(ctx);
-  requestAnimationFrame(gameLoop);
+  if(boss1.active){
+    player.update();
+    player.draw(ctx);
+    checkBossCollision();
+    checkBrickCollision();
+    boss1.update();
+    ball.update();
+    ball.draw(ctx);
+    requestAnimationFrame(gameLoop);
+  } else {
+      player.update();
+      player.draw(ctx);
+      checkBrickCollision();
+      drawAllBricks(bricksArr);
+      ball.update();
+      ball.draw(ctx);
+      requestAnimationFrame(gameLoop);
+  }
+
 }
-
-
-
+}
