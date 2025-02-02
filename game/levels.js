98fcd69ed4da555bc31a124f4f2c256f855716da
tjlearnscode <121098526+tjlearnscode@users.noreply.github.com>
2025-02-02 11:22:28 -0500

    Update levels.js -added level 3

--- game/levels.js
+++ game/levels.js
@@ -6,6 +6,11 @@ export let levels = [
 {
   "levelNumber": 2,
   "brickColor": "light-greens",
-  "grayIndexes": [22,23,24,25,26,27]
+  "greyIndexes": [22,23,24,25,26,27]
+},
+{
+  "levelNumber": 3,
+  "brickColor": "purples",
+  "greyIndexes": [22,23,26,27,31,32,35,36]
 }
 ]
